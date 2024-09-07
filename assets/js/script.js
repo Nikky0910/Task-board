ca9ebe1fc3cc44835db4385fa6c43cd141fc5e08
Marianna <mariannaniko1092@gmail.com>
2024-09-07 18:15:45 -0400

    Add modal and form to HTML

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,11 @@
+
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
-
+    
 }
 
 // Todo: create a function to create a task card
